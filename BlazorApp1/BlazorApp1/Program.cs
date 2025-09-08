2cfad8a2ef3df0640590641ae26824ba8139cf9e
Nicolae-Bivol <nicolae.bivol@isa.utm.md>
2025-09-08 15:00:30 +0300

    Add project files.

---
+++ BlazorApp1/BlazorApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using BlazorApp1.Client.Pages;
+using BlazorApp1.Components;
+using BlazorApp1.Components.Account;
+using BlazorApp1.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+builder.Services.AddAuthentication(options =>
+{
+     options.DefaultScheme = IdentityConstants.ApplicationScheme;
+     options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
+
+// 🔹 Connection string din appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// 🔹 Înregistrăm DbContextFactory în loc de DbContext
+builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString)
+           .EnableSensitiveDataLogging(builder.Environment.IsDevelopment()));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// 🔹 Identity folosește DbContext prin factory
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+     app.UseWebAssemblyDebugging();
+     app.UseMigrationsEndPoint();
+}
+else
+{
+     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+     app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorApp1.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalIdentityEndpoints();
+
+app.Run();
